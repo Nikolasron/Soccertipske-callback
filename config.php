d038de530e0620d09e3a8ac947eb1b773e077224
Nikolasron <nicholasrono444@gmail.com>
2025-12-04 19:57:52 +0300

    Add Paystack configuration to config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Paystack keys from Render environment
+$PAYSTACK_SECRET = getenv("PAYSTACK_SECRET_KEY");
+$PAYSTACK_PUBLIC = getenv("PAYSTACK_PUBLIC_KEY");
+
+// Frontend URL for redirecting user after payment
+$FRONTEND_URL = "https://soccertipske.wuaze.com";
+
+?>
