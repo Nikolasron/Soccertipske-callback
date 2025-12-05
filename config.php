2f0ebba2bea7559231d16106b5ff6d5817a9f847
Nikolasron <nicholasrono444@gmail.com>
2025-12-05 18:06:05 +0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Paystack keys from Render environment
-$PAYSTACK_SECRET = getenv("PAYSTACK_SECRET_KEY");
-$PAYSTACK_PUBLIC = getenv("PAYSTACK_PUBLIC_KEY");
-
-// Frontend URL for redirecting user after payment
-$FRONTEND_URL = "https://soccertipske.wuaze.com";
-
-?>
