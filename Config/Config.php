408b207a9b2ccf67e04c8225f57a081626ff714a
Nikolasron <nicholasrono444@gmail.com>
2025-12-06 10:15:00 +0300

    Delete Config directory

--- Config/Config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Paystack live keys (SECURED)
-define('PAYSTACK_SECRET_KEY', 'sk_live_337187d04a2fc4d44d650d6e0b1877d4b0e26119');
-define('PAYSTACK_PUBLIC_KEY', 'pk_live_8a9d1734002c31e7a5168107c7f3193b48314f6a');
-
-// Main callback URL
-define('CALLBACK_URL', 'https://soccertipske.wuaze.com/index.php');
